0411e123ad1d970db34d56f44ef0afc74a5323b8
HanIn99 <55255115+HanIn99@users.noreply.github.com>
2020-05-01 17:30:30 -0700

    Create cname.py

---
+++ cname.py
@@ -0,0 +1,25 @@
+import sys
+import dns
+import dns.resolver
+import colorama
+import time
+from colorama import Fore, Style
+from requests.exceptions import ConnectionError
+
+
+sublist=sys.argv[1]
+my_resolver = dns.resolver.Resolver()
+my_resolver.nameservers = ['8.8.8.8']
+subfile=open(sublist, 'r')
+for sub in subfile:
+	try:
+		subd=sub.strip()		
+		answer=my_resolver.query(sub.strip(), 'CNAME')
+		for data in answer:
+			print((subd))
+			print( "cname: " + str(data) +"\n")
+	except :    
+   		pass 
+   		
+subfile.close()
+
